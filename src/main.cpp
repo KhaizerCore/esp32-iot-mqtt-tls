c881f940dcb8d206dd35434db558bc824ddf55e7
KhaizerCore <gustavo.werkhaizer@hotmail.com>
2022-08-16 21:37:43 -0300

    stable version

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,8 @@ using namespace std;
 
 DigitalOutput builtInLED(LED_BUILTIN, 1, 10, true);
 
+/*
+// cert hivemq
 const char* ca_cert = \ 
 "-----BEGIN CERTIFICATE-----\n" \
 "MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw\n" \
@@ -48,7 +50,83 @@ const char* ca_cert = \
 "mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d\n" \
 "emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=\n" \
 "-----END CERTIFICATE-----\n";
+*/
 
+//cert emqx broker
+const char ca_cert[] PROGMEM = R"=====(
+-----BEGIN CERTIFICATE-----
+MIIF3jCCA8agAwIBAgIQAf1tMPyjylGoG7xkDjUDLTANBgkqhkiG9w0BAQwFADCB
+iDELMAkGA1UEBhMCVVMxEzARBgNVBAgTCk5ldyBKZXJzZXkxFDASBgNVBAcTC0pl
+cnNleSBDaXR5MR4wHAYDVQQKExVUaGUgVVNFUlRSVVNUIE5ldHdvcmsxLjAsBgNV
+BAMTJVVTRVJUcnVzdCBSU0EgQ2VydGlmaWNhdGlvbiBBdXRob3JpdHkwHhcNMTAw
+MjAxMDAwMDAwWhcNMzgwMTE4MjM1OTU5WjCBiDELMAkGA1UEBhMCVVMxEzARBgNV
+BAgTCk5ldyBKZXJzZXkxFDASBgNVBAcTC0plcnNleSBDaXR5MR4wHAYDVQQKExVU
+aGUgVVNFUlRSVVNUIE5ldHdvcmsxLjAsBgNVBAMTJVVTRVJUcnVzdCBSU0EgQ2Vy
+dGlmaWNhdGlvbiBBdXRob3JpdHkwggIiMA0GCSqGSIb3DQEBAQUAA4ICDwAwggIK
+AoICAQCAEmUXNg7D2wiz0KxXDXbtzSfTTK1Qg2HiqiBNCS1kCdzOiZ/MPans9s/B
+3PHTsdZ7NygRK0faOca8Ohm0X6a9fZ2jY0K2dvKpOyuR+OJv0OwWIJAJPuLodMkY
+tJHUYmTbf6MG8YgYapAiPLz+E/CHFHv25B+O1ORRxhFnRghRy4YUVD+8M/5+bJz/
+Fp0YvVGONaanZshyZ9shZrHUm3gDwFA66Mzw3LyeTP6vBZY1H1dat//O+T23LLb2
+VN3I5xI6Ta5MirdcmrS3ID3KfyI0rn47aGYBROcBTkZTmzNg95S+UzeQc0PzMsNT
+79uq/nROacdrjGCT3sTHDN/hMq7MkztReJVni+49Vv4M0GkPGw/zJSZrM233bkf6
+c0Plfg6lZrEpfDKEY1WJxA3Bk1QwGROs0303p+tdOmw1XNtB1xLaqUkL39iAigmT
+Yo61Zs8liM2EuLE/pDkP2QKe6xJMlXzzawWpXhaDzLhn4ugTncxbgtNMs+1b/97l
+c6wjOy0AvzVVdAlJ2ElYGn+SNuZRkg7zJn0cTRe8yexDJtC/QV9AqURE9JnnV4ee
+UB9XVKg+/XRjL7FQZQnmWEIuQxpMtPAlR1n6BB6T1CZGSlCBst6+eLf8ZxXhyVeE
+Hg9j1uliutZfVS7qXMYoCAQlObgOK6nyTJccBz8NUvXt7y+CDwIDAQABo0IwQDAd
+BgNVHQ4EFgQUU3m/WqorSs9UgOHYm8Cd8rIDZsswDgYDVR0PAQH/BAQDAgEGMA8G
+A1UdEwEB/wQFMAMBAf8wDQYJKoZIhvcNAQEMBQADggIBAFzUfA3P9wF9QZllDHPF
+Up/L+M+ZBn8b2kMVn54CVVeWFPFSPCeHlCjtHzoBN6J2/FNQwISbxmtOuowhT6KO
+VWKR82kV2LyI48SqC/3vqOlLVSoGIG1VeCkZ7l8wXEskEVX/JJpuXior7gtNn3/3
+ATiUFJVDBwn7YKnuHKsSjKCaXqeYalltiz8I+8jRRa8YFWSQEg9zKC7F4iRO/Fjs
+8PRF/iKz6y+O0tlFYQXBl2+odnKPi4w2r78NBc5xjeambx9spnFixdjQg3IM8WcR
+iQycE0xyNN+81XHfqnHd4blsjDwSXWXavVcStkNr/+XeTWYRUc+ZruwXtuhxkYze
+Sf7dNXGiFSeUHM9h4ya7b6NnJSFd5t0dCy5oGzuCr+yDZ4XUmFF0sbmZgIn/f3gZ
+XHlKYC6SQK5MNyosycdiyA5d9zZbyuAlJQG03RoHnHcAP9Dc1ew91Pq7P8yF1m9/
+qS3fuQL39ZeatTXaw2ewh0qpKJ4jjv9cJ2vhsE/zB+4ALtRZh8tSQZXq9EfX7mRB
+VXyNWQKV3WKdwrnuWih0hKWbt5DHDAff9Yk2dDLWKMGwsAvgnEzDHNb842m1R0aB
+L6KCq9NjRHDEjf8tM7qtj3u1cIiuPhnPQCjY/MiQu12ZIvVS5ljFH4gxQ+6IHdfG
+jjxDah2nGN59PRbxYvnKkKj9
+-----END CERTIFICATE-----
+)=====";
+
+const char jsonBoardData[] PROGMEM = R"=====(
+{
+  "license_key": "347848d2-757c-4861-a3e5-c8e12e3c538d",
+  "device_setup": [
+    {
+      "TYPE": "VARIABLE",
+      "VARIABLE_NAME": "virtual switch",
+      "NAME": "Switch",
+      "PIN": -1,
+      "CODE": "SWITCH",
+      "VALUE": true,
+      "VALUE_TYPE": "BOOL",
+      "TOPIC_ID": "7ffa7884-b40b-4ac5-9324-54f36c038192"
+    },
+    {
+      "TYPE": "INPUT",
+      "VARIABLE_NAME": "",
+      "NAME": "DHT22",
+      "PIN": 2,
+      "CODE": "DHT22",
+      "VALUE": 0,
+      "VALUE_TYPE": "float",
+      "TOPIC_ID": "1609c623-9387-4eb2-b6e4-af1b9a15061c"
+    },
+    {
+      "TYPE": "OUTPUT",
+      "VARIABLE_NAME": "",
+      "NAME": "Luz da lampada",
+      "PIN": 5,
+      "CODE": "LED",
+      "VALUE": true,
+      "VALUE_TYPE": "BOOL",
+      "TOPIC_ID": "a22ab6e6-6ff1-4164-af49-83a372db5e2a"
+    }
+  ]
+}
+)=====";
 
 //WiFiClient espClient;
 WiFiClientSecure net;
@@ -103,8 +181,6 @@ void reconnect(){
     // Attempt to connect
     if (client.connect(clientId.c_str(), username.c_str(), password.c_str())) {
       Serial.println("connected");
-      // Once connected, publish an announcement...
-      client.publish("outTopic", "hello world");
       // ... and resubscribe
       client.subscribe("inTopic");
     } else {
@@ -156,7 +232,12 @@ void setup() {
 
   // mqtts things
   net.setCACert(ca_cert);
-  client.setServer("2eddfd9c7eba4f5a8fa6cc3d402240e3.s1.eu.hivemq.cloud", 8883);
+  // hivemq
+  //client.setServer("2eddfd9c7eba4f5a8fa6cc3d402240e3.s1.eu.hivemq.cloud", 8883);
+  
+  //emqx
+  client.setServer("broker.emqx.io", 8883);
+
   client.setCallback(callback);
   
   reconnect();
@@ -172,12 +253,9 @@ void loop() {
   }
   
 
-  Serial.println("Publishing hello world: ");
-  client.publish("/hello", "world");
-
   
 
-  vTaskDelay(1000 / portTICK_PERIOD_MS);  // wait for a second       
+  //vTaskDelay(5000 / portTICK_PERIOD_MS);  // wait for a second       
 }
 
 
